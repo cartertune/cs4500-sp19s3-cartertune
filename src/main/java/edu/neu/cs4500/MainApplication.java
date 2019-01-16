4031131e1f4562ba8f14bef1c7a53402d9e4197f
Carter Tune <cartertune@Carters-MacBook-Pro.local>
2019-01-16 16:50:28 -0500

    first commit

---
+++ src/main/java/edu/neu/cs4500/MainApplication.java
@@ -0,0 +1,14 @@
+package edu.neu.cs4500;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+}
+
